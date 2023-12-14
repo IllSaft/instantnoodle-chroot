473d006b560d9c5affa575fbccd77ece65dc2ac8
Scott Howson <scott_howson@corruptiongames.net>
2023-12-14 13:07:00 -0500

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+CONT=/data
 
+for i in dev dev/pts sys proc; do
+    mount --bind /$i $CONT/$i
+done
+
+chroot=$(which chroot)
+PATH="/usr/bin:/usr/sbin:/usr/local/bin" $chroot $CONT $@
+
+for i in dev/pts dev sys proc; do
+    umount $CONT/$i
+done
