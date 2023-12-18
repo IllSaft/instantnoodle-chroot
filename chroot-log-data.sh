a3fe6d6cc8f5b4766511bb4396b7d12cc69a03ea
Scott Howson <scott_howson@corruptiongames.net>
2023-12-18 10:34:33 -0500

    Updated chroot scripts

--- chroot-log-data.sh
+++ chroot-log-data.sh
@@ -1,105 +1,157 @@
 #!/bin/bash
 
+# ANSI color codes
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+
+# Bold ANSI color codes
+BOLD_BLACK='\033[1;30m'
+BOLD_RED='\033[1;31m'
+BOLD_GREEN='\033[1;32m'
+BOLD_YELLOW='\033[1;33m'
+BOLD_BLUE='\033[1;34m'
+BOLD_MAGENTA='\033[1;35m'
+BOLD_CYAN='\033[1;36m'
+BOLD_WHITE='\033[1;37m'
+
+# Background ANSI color codes
+BG_BLACK='\033[40m'
+BG_RED='\033[41m'
+BG_GREEN='\033[42m'
+BG_YELLOW='\033[43m'
+BG_BLUE='\033[44m'
+BG_MAGENTA='\033[45m'
+BG_CYAN='\033[46m'
+BG_WHITE='\033[47m'
+
+# Reset ANSI color code
+NC='\033[0m' # No Color
+
+
 # Define a log file
 LOG_FILE="/mnt/ubuntu/mount_script.log"
 
-# Function to add log entries
+# Decorative divider
+divider() {
+    echo -e "${BOLD_CYAN}——————————————————————————————————————————————————————————————————————————————————${NC}"
+}
+# Yellow Decorative divider
+yellow_divider() {
+    echo -e "${BOLD_YELLOW}——————————————————————————————————————————————————————————————————————————————————${NC}"
+}
+# Magenta Decorative divider
+magenta_divider() {
+    echo -e "${BOLD_MAGENTA}——————————————————————————————————————————————————————————————————————————————————${NC}"
+}
+
+
+# Function to add log entries with color
 log() {
-    echo "$(date '+%Y-%m-%d %H:%M:%S'): $@" | tee -a $LOG_FILE
+    local message="$1"
+    local color="$2"
+    echo -e "${YELLOW}$(date '+%b/%d/%Y') — ${BOLD_BLUE}$(date '+%-l:%M %p'):${NC} ${color}${message}${NC}" | tee -a $LOG_FILE
 }
 
 # Start the script 
-
-echo "$(date): Entering Chroot Environment..." >> $LOG_FILE
+log "Executing Chroot Environment Scripts..." "$BOLD_MAGENTA"
 
 # Mount the filesystem
-mkdir -p /mnt/ubuntu && log "Created /mnt/ubuntu directory." || log "Failed to create /mnt/ubuntu directory."
+mkdir -p /mnt/ubuntu && log "✔️ Created /mnt/ubuntu directory." "$GREEN" || log "❌ Failed to create /mnt/ubuntu directory." "$RED"
 
 if mount -o loop /data/ubuntu.img /mnt/ubuntu; then
-    log "Mounted /data/ubuntu.img to /mnt/ubuntu."
+    log "✔️ Mounted /data/ubuntu.img to /mnt/ubuntu." "$GREEN"
 else
-    log "Failed to mount /data to /mnt/ubuntu."
+    log "❌ Failed to mount /data to /mnt/ubuntu." "$RED"
     exit 1
 fi
 
+divider
+
 # Create a custom /etc/group file in the chroot environment
-echo "Creating custom /etc/group file in the chroot environment."
-echo "group1:x:1007:" > /mnt/ubuntu/etc/group
-echo "group2:x:1011:" >> /mnt/ubuntu/etc/group
-echo "group3:x:1028:" >> /mnt/ubuntu/etc/group
-echo "group4:x:1078:" >> /mnt/ubuntu/etc/group
-echo "group5:x:1079:" >> /mnt/ubuntu/etc/group
-echo "group6:x:3001:" >> /mnt/ubuntu/etc/group
-echo "group7:x:3006:" >> /mnt/ubuntu/etc/group
-echo "group8:x:3009:" >> /mnt/ubuntu/etc/group
-echo "group9:x:3011:" >> /mnt/ubuntu/etc/group
-echo "group10:x:0:" >> /mnt/ubuntu/etc/group
-echo "group11:x:1004:" >> /mnt/ubuntu/etc/group
-echo "group12:x:1015:" >> /mnt/ubuntu/etc/group
-echo "group13:x:3002:" >> /mnt/ubuntu/etc/group
-echo "group14:x:3003:" >> /mnt/ubuntu/etc/group
-log "Custom /etc/group file created."
+log "Creating custom /etc/group file in the chroot environment..." "$BOLD_MAGENTA"
+group_ids=(1007 1011 1028 1078 1079 3001 3006 3009 3011 0 1004 1015 3002 3003)
+for i in {0..13}; do
+    echo "group$(($i + 1)):x:${group_ids[$i]}:" >> /mnt/ubuntu/etc/group
+done
+log "✔️ Custom /etc/group file created." "$GREEN"
+
+divider
 
 # Set the PATH environment variable
 export PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin
-log "Set PATH environment variable."
+log "Setting PATH environment variables..." "$BOLD_MAGENTA"
 
 # Bind mount important directories
 if mount --bind /dev /mnt/ubuntu/dev && 
    mount --bind /dev/pts /mnt/ubuntu/dev/pts && 
    mount --bind /sys /mnt/ubuntu/sys && 
    mount --bind /proc /mnt/ubuntu/proc; then
-    log "Bind mounted important directories."
+    log "✔️ Bind mounted important directories." "$GREEN"
 else
-    log "Failed to bind mount important directories."
+    log "❌ Failed to bind mount important directories." "$RED"
     exit 1
 fi
 
+divider
+
 # Enter the chroot environment
-echo "$(date): Entered chroot environment." >> $LOG_FILE
+log "Entering chroot environment..." "$YELLOW"
+
 chroot /mnt/ubuntu /bin/bash
+
+log "Exiting chroot environment..." "$YELLOW"
+
+divider
 
 # The following will execute after you exit the chroot environment
 
 # Check disk usage and report if it's over a certain threshold
+storage_usage=$(df -h | grep '/mnt/ubuntu' | awk '{print $5}')
+
+# Log storage use percentage
+log "${YELLOW}Storage usage on /mnt/ubuntu: ${MAGENTA}$storage_usage${RESET}"
+
+# Warning for reaching 85% storage usage.
 df -h | grep '/mnt/ubuntu' | awk '{print $5}' | while read -r usage; do
-    if [ "${usage%*%}" -gt 75 ]; then
-        log "Warning: Disk usage is over 75% on /mnt/ubuntu"
+    if [ "${usage%*%}" -gt 85 ]; then
+        log "❌ Warning: Disk usage is over 85% on /mnt/ubuntu" "$RED"
     fi
 done
 
-# Log storage use percentage
-storage_usage=$(df -h | grep '/mnt/ubuntu' | awk '{print $5}')
-log "Storage usage on /mnt/ubuntu: $storage_usage"
+divider
 
 # Log users in home directory
 if [ -d "/mnt/ubuntu/home" ]; then
-    log "Users in /mnt/ubuntu/home:"
+    log "Users in /mnt/ubuntu/home:" ${YELLOW}
     for user_home in /mnt/ubuntu/home/*; do
         if [ -d "$user_home" ]; then
             user=$(basename "$user_home")
-            log " - $user"
+            log "✔️ - $user" "$GREEN"
         fi
     done
 else
-    log "Home directory /mnt/ubuntu/home not found."
+    log "Home directory /mnt/ubuntu/home not found." "$RED"
 fi
 
-# Create custom logs
-echo "$(date): Exited chroot environment" >> $LOG_FILE
+divider
 
 # Check for specific files and take action
 if [ -f /mnt/ubuntu/bin/sh ]; then
-    log "Found bin/sh."
+    log "✔️ Found bin/sh." "$GREEN"
 else
-    log "bin/sh not found. ERROR!"
+    log "bin/sh not found. ERROR!" "$RED"
 fi
 
-# Run custom scripts or commands
-# log "Running custom scripts or commands."
-# /mnt/ubuntu/custom_script.sh
+divider
+
 # Remove the custom /etc/group file
-rm -f /mnt/ubuntu/etc/group && log "Custom /etc/group file removed."
+rm -f /mnt/ubuntu/etc/group && log "✔️ Custom /etc/group file deleted." "$GREEN"
 
 # Cleanup: unmount bound directories
 if umount /mnt/ubuntu/dev/pts && 
@@ -107,9 +159,12 @@ if umount /mnt/ubuntu/dev/pts &&
    umount /mnt/ubuntu/sys && 
    umount /mnt/ubuntu/proc && 
    umount /mnt/ubuntu; then
-    log "Unmounted all directories."
+    log "Unmounted all directories." "$BOLD_MAGENTA"
 else
-    log "Failed to unmount some directories."
+    log "❌ Failed to unmount some directories." "$RED"
 fi
 
-log "Chroot Environment Exited Successfully."
+divider
+log "✔️ Chroot Environment Exited Successfully." "$BOLD_MAGENTA"
+yellow_divider
+log "${MAGENTA}Good-bye. ${RESET}👋"
--- chroot-log-system.sh
+++ chroot-log-system.sh
@@ -1,104 +1,149 @@
 #!/bin/bash
 
+# ANSI color codes
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+
+# Bold ANSI color codes
+BOLD_BLACK='\033[1;30m'
+BOLD_RED='\033[1;31m'
+BOLD_GREEN='\033[1;32m'
+BOLD_YELLOW='\033[1;33m'
+BOLD_BLUE='\033[1;34m'
+BOLD_MAGENTA='\033[1;35m'
+BOLD_CYAN='\033[1;36m'
+BOLD_WHITE='\033[1;37m'
+
+# Background ANSI color codes
+BG_BLACK='\033[40m'
+BG_RED='\033[41m'
+BG_GREEN='\033[42m'
+BG_YELLOW='\033[43m'
+BG_BLUE='\033[44m'
+BG_MAGENTA='\033[45m'
+BG_CYAN='\033[46m'
+BG_WHITE='\033[47m'
+
+# Reset ANSI color code
+NC='\033[0m' # No Color
+
+
 # Define a log file
 LOG_FILE="/mnt/system/mount_script.log"
 
-# Function to add log entries
-log() {
-    echo "$(date '+%Y-%m-%d %H:%M:%S'): $@" | tee -a $LOG_FILE
+# Decorative divider
+divider() {
+    echo -e "${BOLD_CYAN}——————————————————————————————————————————————————————————————————————————————————${NC}"
+}
+# Yellow Decorative divider
+yellow_divider() {
+    echo -e "${BOLD_YELLOW}——————————————————————————————————————————————————————————————————————————————————${NC}"
+}
+# Magenta Decorative divider
+magenta_divider() {
+    echo -e "${BOLD_MAGENTA}——————————————————————————————————————————————————————————————————————————————————${NC}"
 }
 
-# Start the script
-echo "$(date): Entering Chroot Environment..." >> $LOG_FILE
+
+# Function to add log entries with color
+log() {
+    local message="$1"
+    local color="$2"
+    echo -e "${YELLOW}$(date '+%b/%d/%Y') — ${BOLD_BLUE}$(date '+%-l:%M %p'):${NC} ${color}${message}${NC}" | tee -a $LOG_FILE
+}
+
+# Start the script 
+log "Executing Chroot Environment Scripts..." "$BOLD_MAGENTA"
 
 # Mount the filesystem
-mkdir -p /mnt/system && log "Created /mnt/system directory." || log "Failed to create /mnt/system directory."
+mkdir -p /mnt/system && log "✔️ Created /mnt/system directory." "$GREEN" || log "❌ Failed to create /mnt/system directory." "$RED"
 
 if mount -o loop /dev/block/dm-0 /mnt/system; then
-    log "Mounted /dev/block/dm-0 to /mnt/system."
+    log "✔️ Mounted /dev/block/dm-0 to /mnt/system." "$GREEN"
 else
-    log "Failed to mount /dev/block/dm-0 to /mnt/system."
+    log "❌ Failed to mount /dev/block/dm-0 to /mnt/system." "$RED"
     exit 1
 fi
 
+divider
+
 # Create a custom /etc/group file in the chroot environment
-echo "Creating custom /etc/group file in the chroot environment."
-echo "group1:x:1007:" > /mnt/system/etc/group
-echo "group2:x:1011:" >> /mnt/system/etc/group
-echo "group3:x:1028:" >> /mnt/system/etc/group
-echo "group4:x:1078:" >> /mnt/system/etc/group
-echo "group5:x:1079:" >> /mnt/system/etc/group
-echo "group6:x:3001:" >> /mnt/system/etc/group
-echo "group7:x:3006:" >> /mnt/system/etc/group
-echo "group8:x:3009:" >> /mnt/system/etc/group
-echo "group9:x:3011:" >> /mnt/system/etc/group
-echo "group10:x:0:" >> /mnt/system/etc/group
-echo "group11:x:1004:" >> /mnt/system/etc/group
-echo "group12:x:1015:" >> /mnt/system/etc/group
-echo "group13:x:3002:" >> /mnt/system/etc/group
-echo "group14:x:3003:" >> /mnt/system/etc/group
-log "Custom /etc/group file created."
+log "Creating custom /etc/group file in the chroot environment..." "$BOLD_MAGENTA"
+group_ids=(1007 1011 1028 1078 1079 3001 3006 3009 3011 0 1004 1015 3002 3003)
+for i in {0..13}; do
+    echo "group$(($i + 1)):x:${group_ids[$i]}:" >> /mnt/system/etc/group
+done
+log "✔️ Custom /etc/group file created." "$GREEN"
+
+divider
 
 # Set the PATH environment variable
 export PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin
-log "Set PATH environment variable."
+log "Setting PATH environment variables..." "$BOLD_MAGENTA"
 
 # Bind mount important directories
 if mount --bind /dev /mnt/system/dev && 
    mount --bind /dev/pts /mnt/system/dev/pts && 
    mount --bind /sys /mnt/system/sys && 
    mount --bind /proc /mnt/system/proc; then
-    log "Bind mounted important directories."
+    log "✔️ Bind mounted important directories." "$GREEN"
 else
-    log "Failed to bind mount important directories."
+    log "❌ Failed to bind mount important directories." "$RED"
     exit 1
 fi
 
+divider
+
 # Enter the chroot environment
-echo "$(date): Entered chroot environment." >> $LOG_FILE
+log "Entering chroot environment..." "$YELLOW"
+
 chroot /mnt/system /bin/bash
 
-# The following will execute after you exit the chroot environment
+log "Exiting chroot environment..." "$YELLOW"
 
-# Check disk usage and report if it's over a certain threshold
+dividerubuntuA}$storage_usage${RESET}"
+
+# Warning for reaching 85% storage usage.
 df -h | grep '/mnt/system' | awk '{print $5}' | while read -r usage; do
-    if [ "${usage%*%}" -gt 75 ]; then
-        log "Warning: Disk usage is over 75% on /mnt/system"
+    if [ "${usage%*%}" -gt 85 ]; then
+        log "❌ Warning: Disk usage is over 85% on /mnt/system" "$RED"
     fi
 done
 
-# Log storage use percentage
-storage_usage=$(df -h | grep '/mnt/system' | awk '{print $5}')
-log "Storage usage on /mnt/system: $storage_usage"
+divider
 
 # Log users in home directory
 if [ -d "/mnt/system/home" ]; then
-    log "Users in /mnt/system/home:"
+    log "Users in /mnt/system/home:" ${YELLOW}
     for user_home in /mnt/system/home/*; do
         if [ -d "$user_home" ]; then
             user=$(basename "$user_home")
-            log " - $user"
+            log "✔️ - $user" "$GREEN"
         fi
     done
 else
-    log "Home directory /mnt/system/home not found."
+    log "Home directory /mnt/system/home not found." "$RED"
 fi
 
-# Create custom logs
-echo "$(date): Exited chroot environment" >> $LOG_FILE
+divider
 
 # Check for specific files and take action
 if [ -f /mnt/system/bin/sh ]; then
-    log "Found bin/sh."
+    log "✔️ Found bin/sh." "$GREEN"
 else
-    log "bin/sh not found. ERROR!"
+    log "bin/sh not found. ERROR!" "$RED"
 fi
 
-# Run custom scripts or commands
-# log "Running custom scripts or commands."
-# /mnt/system/custom_script.sh
+divider
+
 # Remove the custom /etc/group file
-rm -f /mnt/system/etc/group && log "Custom /etc/group file removed."
+rm -f /mnt/system/etc/group && log "✔️ Custom /etc/group file deleted." "$GREEN"
 
 # Cleanup: unmount bound directories
 if umount /mnt/system/dev/pts && 
@@ -106,9 +151,12 @@ if umount /mnt/system/dev/pts &&
    umount /mnt/system/sys && 
    umount /mnt/system/proc && 
    umount /mnt/system; then
-    log "Unmounted all directories."
+    log "Unmounted all directories." "$BOLD_MAGENTA"
 else
-    log "Failed to unmount some directories."
+    log "❌ Failed to unmount some directories." "$RED"
 fi
 
-log "Chroot Environment Exited Successfully."
+divider
+log "✔️ Chroot Environment Exited Successfully." "$BOLD_MAGENTA"
+yellow_divider
+log "${MAGENTA}Good-bye. ${RESET}👋"
