443ba1f53b6bee23c33e37b9ad31cd12b23a8616
Scott Howson <scott_howson@corruptiongames.net>
2023-12-19 17:09:07 -0500

    Update chroot-log-data.sh

--- chroot-log-data.sh
+++ chroot-log-data.sh
@@ -33,21 +33,20 @@ BG_WHITE='\033[47m'
 # Reset ANSI color code
 NC='\033[0m' # No Color
 
-
 # Define a log file
-LOG_FILE="/mnt/ubuntu/mount_script.log"
+LOG_FILE="/mnt/system/mount_script.log"
 
 # Decorative divider
 divider() {
-    echo -e "${BOLD_CYAN}——————————————————————————————————————————————————————————————————————————————————${NC}"
+    echo -e "${BOLD_CYAN}————————————————————————————————————————————————————————————————————————————————${NC}"
 }
 # Yellow Decorative divider
 yellow_divider() {
-    echo -e "${BOLD_YELLOW}——————————————————————————————————————————————————————————————————————————————————${NC}"
+    echo -e "${BOLD_YELLOW}————————————————————————————————————————————————————————————————————————————————${NC}"
 }
 # Magenta Decorative divider
 magenta_divider() {
-    echo -e "${BOLD_MAGENTA}——————————————————————————————————————————————————————————————————————————————————${NC}"
+    echo -e "${BOLD_MAGENTA}————————————————————————————————————————————————————————————————————————————————${NC}"
 }
 
 
@@ -55,19 +54,65 @@ magenta_divider() {
 log() {
     local message="$1"
     local color="$2"
-    echo -e "${YELLOW}$(date '+%b/%d/%Y') — ${BOLD_BLUE}$(date '+%-l:%M %p'):${NC} ${color}${message}${NC}" | tee -a $LOG_FILE
+    echo -e "${YELLOW}$(date '+%b/%d/%Y') — ${BOLD_BLUE}$(date '+%-l:%M %p'):${NC} ${color}${message}${NC}" | tee -a "$LOG_FILE"
 }
+
+# Function to get and display the product model
+get_product_model() {
+    local model
+    model=$(getprop ro.product.model)
+    log "Device: $model" "$BOLD_GREEN"
+
+    local processor
+    processor=$(cat /proc/cpuinfo | grep 'Processor' | cut -d ':' -f 2 | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
+    log "Processor: $processor" "$BOLD_GREEN"
+}
+
+# Function to get and display the number of processor cores using adb
+get_processor_cores() {
+    local num_cores
+    num_cores=$(cat /proc/cpuinfo | grep 'processor' | wc -l)
+    log "Cores: ${num_cores} Cores" "$BOLD_GREEN"
+}
+
+# Function to get and display memory information in GB
+get_memory_info() {
+    local total_mem_kb
+    total_mem_kb=$(cat /proc/meminfo | grep 'MemTotal' | awk '{print $2}')
+    total_mem_gb=$(echo "scale=2; $total_mem_kb / 1024 / 1024" | bc)
+    log "Total Memory: ${total_mem_gb} GB" "$BOLD_GREEN"
+}
+
 
 # Start the script 
 log "Executing Chroot Environment Scripts..." "$BOLD_MAGENTA"
+divider
+log "Getting Device Info..." "$BOLD_MAGENTA"
+get_product_model
+get_processor_cores
+get_memory_info
+divider
 
 # Mount the filesystem
-mkdir -p /mnt/ubuntu && log "✔️ Created /mnt/ubuntu directory." "$GREEN" || log "❌ Failed to create /mnt/ubuntu directory." "$RED"
-
+log "Mounting the System Partition..." "$BOLD_MAGENTA"
 if mount -o loop /data/ubuntu.img /mnt/ubuntu; then
     log "✔️ Mounted /data/ubuntu.img to /mnt/ubuntu." "$GREEN"
+
+    # Update /etc/fstab
+    FSTAB_LINE="/data/ubuntu.img /mnt/ubuntu ext4 rw 0 0"
+    if grep -Fxq "$FSTAB_LINE" /etc/fstab; then
+        log "✔️ Line already exists in /etc/fstab." "$GREEN"
+    else
+        echo "$FSTAB_LINE" | tee -a /etc/fstab > /dev/null
+        if grep -Fxq "$FSTAB_LINE" /etc/fstab; then
+            log "✔️ Line successfully added to /etc/fstab." "$GREEN"
+        else
+            log "❌ Failed to add line to /etc/fstab. Check permissions and path." "$RED"
+        fi
+    fi
+
 else
-    log "❌ Failed to mount /data to /mnt/ubuntu." "$RED"
+    log "❌ Failed to mount /data/ubuntu.img to /mnt/ubuntu." "$RED"
     exit 1
 fi
 
